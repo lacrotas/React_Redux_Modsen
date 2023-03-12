4236745e090f3510d9c9bee844be4f96d6812236
lacrotas <123123maks1@mail.ru>
2023-03-13 02:43:11 +0300

    LoadMoreButton_LoadingAnimation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <SearchSection />
-        <BookResultGrid /> 
+        {/* <BookResultGrid />  */}
       </div>
     </Provider>
   );
---
+++ src/components/LoadingModalWindow/LoadingModalWindow.js
@@ -0,0 +1,15 @@
+import './LoadingModalWindow.scss';
+
+function LoadingModalWindow() {
+
+    return (
+        <div className="LoadingModalWindow">
+            <div className="spinner">
+            </div>
+        </div>
+    );
+}
+
+
+export default LoadingModalWindow;
+
